74450f36974f46c96089c0ecb0255edeab647555
Matheus <matheus.g.almeida21@gmail.com>
2022-08-15 17:57:59 -0300

    feat: add tool entity

---
+++ src/core/entities/tools.ts
@@ -0,0 +1,13 @@
+export default class Tool {
+    title: string;
+    link: string;
+    description: string;
+    tags: string[];
+
+    constructor(title: string, link: string, description: string, tags: string[]) {
+        this.title = title
+        this.link = link
+        this.description = description
+        this.tags = tags
+    }
+}
