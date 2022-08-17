07af97e7a01a5b56ca1c8199546ce1a52f17e091
Matheus <matheus.g.almeida21@gmail.com>
2022-08-17 11:05:03 -0300

    test: add unit test to DeleteTool useCase

---
+++ tests/unit/deleteTool.spec.ts
@@ -0,0 +1,27 @@
+import ToolFactory from "../../src/core/factories/toolFactory";
+import DeleteTool from "../../src/core/useCase/deleteTool.useCase";
+import GetAllTools from "../../src/core/useCase/getAllTools.useCase";
+import GetTool from "../../src/core/useCase/getTool.useCase";
+import ToolRepositoryMemory from "../../src/infra/repositories/in-memory/tool.repository";
+
+describe("Unit tests to deleteTool useCase", () => {
+    it("Should be able to delete a tool from DB", async () => {
+        const memoryRepo = new ToolRepositoryMemory();
+        const getTool = new GetTool(memoryRepo);
+        const getAllTools = new GetAllTools(memoryRepo);
+        const deleteTool = new DeleteTool(memoryRepo);
+
+        const { id, title } = await ToolFactory(memoryRepo);
+
+        const tool = await getTool.exec(id as number);
+
+        expect(tool.id).toEqual(id);
+        expect(tool.title).toStrictEqual(title);
+
+        await deleteTool.exec(id as number);
+
+        const dbAfterDelete = await getAllTools.exec();
+
+        expect(dbAfterDelete.length).toBe(0);
+    });
+});
