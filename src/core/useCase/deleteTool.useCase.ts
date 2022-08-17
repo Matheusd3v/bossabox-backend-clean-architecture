99985770d85968d8c5dd44952010c5f87fb2fa9d
Matheus <matheus.g.almeida21@gmail.com>
2022-08-17 11:04:27 -0300

    feat: add useCase DeleteTool

---
+++ src/core/useCase/deleteTool.useCase.ts
@@ -0,0 +1,15 @@
+import ToolRepository from "../repositories/tool.repository";
+
+export default class DeleteTool {
+    constructor(private readonly repository: ToolRepository) {}
+
+    public async exec(id: number) {
+        const tool = await this.repository.getToolById(id);
+
+        if (!tool.id) {
+            throw new Error("Tool not nound");
+        }
+
+        await this.repository.deleteTool(id);
+    }
+}
