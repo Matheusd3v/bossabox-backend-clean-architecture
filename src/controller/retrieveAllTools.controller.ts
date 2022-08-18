fc717fa0f2ffaf3c28e428086ae3b94abacfd084
Matheus <matheus.g.almeida21@gmail.com>
2022-08-18 15:17:51 -0300

    feat: add query string to route retrieve all tools, when tag is request

--- src/controller/retrieveAllTools.controller.ts
+++ src/controller/retrieveAllTools.controller.ts
@@ -1,9 +1,19 @@
 import GetAllTools from "../core/useCase/getAllTools.useCase";
+import GetToolsByTag from "../core/useCase/getToolsByTag.useCase";
 import ToolRepositorySqlite from "../infra/repositories/sqlite/toolSQL.repository";
 
 export default class RetrieveAllToolsController {
-    static async exec() {
+    static async exec(_: any, __: any, qs: any) {
+        const { tag } = qs;
         const repository = new ToolRepositorySqlite();
+
+        if (tag) {
+            const getTagedTools = new GetToolsByTag(repository);
+            const toolsWithTag = await getTagedTools.exec(tag);
+
+            return toolsWithTag;
+        }
+
         const getTools = new GetAllTools(repository);
         const tools = await getTools.exec();
 
