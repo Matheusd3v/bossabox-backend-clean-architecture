bf349699b31e6d62352ba2474cacf337cadd5a19
Matheus <matheus.g.almeida21@gmail.com>
2023-04-06 07:25:54 -0300

    feat: add middleware in tool routes

--- src/infra/http/express/routes/tool/index.ts
+++ src/infra/http/express/routes/tool/index.ts
@@ -6,22 +6,32 @@ import RetrieveAllToolsController from "@controller/tool/retrieveAllTools.contro
 import RetrieveToolController from "@controller/tool/retrieveTool.controller";
 import SaveToolController from "@controller/tool/saveTool.controller";
 import UpdateToolController from "@controller/tool/updateTool.controller";
+import { validateRouteParam } from "../../middlewares/route-param-validator.middleware";
 
 const toolRoutes = (app: Express) => {
     const route = Router();
 
     route.post("/tools", ExpressAdapter.create(SaveToolController.exec));
 
-    route.get("/tools/:id", ExpressAdapter.create(RetrieveToolController.exec));
+    route.get(
+        "/tools/:id",
+        validateRouteParam,
+        ExpressAdapter.create(RetrieveToolController.exec)
+    );
 
     route.get("/tools", ExpressAdapter.create(RetrieveAllToolsController.exec));
 
     route.delete(
         "/tools/:id",
+        validateRouteParam,
         ExpressAdapter.create(DeleteToolController.exec)
     );
 
-    route.patch("/tools/:id", ExpressAdapter.create(UpdateToolController.exec));
+    route.patch(
+        "/tools/:id",
+        validateRouteParam,
+        ExpressAdapter.create(UpdateToolController.exec)
+    );
 
     app.use(route);
 };
