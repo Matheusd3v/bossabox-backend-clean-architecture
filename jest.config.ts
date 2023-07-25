7276eb77707400105e93e6a4b45fe094ccda0977
Matheus <matheus.g.almeida21@gmail.com>
2023-07-25 18:51:02 -0300

    test: fix tests module name mapper

--- jest.config.ts
+++ jest.config.ts
@@ -1,24 +1,22 @@
 export default {
-    coverageProvider: "v8",
-    preset: "ts-jest",
-    transform: { "^.+\\.ts?$": "ts-jest" },
-    coverageReporters: ["lcov", "text"],
-    coverageThreshold: {
-      global: {
-        branches: 80,
-        functions: 80,
-        lines: 80,
-        statements: 80,
-      },
+  coverageProvider: "v8",
+  preset: "ts-jest",
+  transform: { "^.+\\.ts?$": "ts-jest" },
+  coverageReporters: ["lcov", "text"],
+  coverageThreshold: {
+    global: {
+      branches: 80,
+      functions: 80,
+      lines: 80,
+      statements: 80,
     },
-    rootDir: "tests",
-    moduleNameMapper: {
-      "^@src/(.*)$": "<rootDir>/../src/$1",
-      "^@adapter/(.*)$": "<rootDir>/../src/adapter/$1",
-      "^@controller/(.*)$": "<rootDir>/../src/controller/$1",
-      "^@core/(.*)$": "<rootDir>/../src/core/$1",
-      "^@infra/(.*)$": "<rootDir>/../src/infra/$1",
-      "^@presentation/(.*)$": "<rootDir>/../src/presentation/$1",
-    }
-
+  },
+  moduleNameMapper: {
+    "^@src/(.*)$": "<rootDir>/src/$1",
+    "^@adapter/(.*)$": "<rootDir>/src/adapter/$1",
+    "^@controller/(.*)$": "<rootDir>/src/controller/$1",
+    "^@core/(.*)$": "<rootDir>/src/core/$1",
+    "^@infra/(.*)$": "<rootDir>/src/infra/$1",
+    "^@presentation/(.*)$": "<rootDir>/src/presentation/$1",
+  }
 };
