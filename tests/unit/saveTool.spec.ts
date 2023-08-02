440d5ca22884797d00f2e22fd99e1d417c147fb2
Matheus <matheus.g.almeida21@gmail.com>
2023-08-02 07:11:50 -0300

    test: ensure business rules to save new tool

--- tests/unit/saveTool.spec.ts
+++ tests/unit/saveTool.spec.ts
@@ -1,6 +1,9 @@
+import { faker } from "@faker-js/faker";
 import ToolFactory from "../../src/core/factories/toolFactory";
 import GetAllTools from "../../src/core/useCase/getAllTools.useCase";
+import SaveTool from "../../src/core/useCase/saveTool.useCase";
 import ToolRepositoryMemory from "../../src/infra/repositories/in-memory/tool.repository";
+import { ConflictError } from "../../src/presentation/Errors";
 
 describe("Unit tests to save tool use case", () => {
     it("Should be able to save a new tool, and return tool from DB", async () => {
@@ -18,4 +21,20 @@ describe("Unit tests to save tool use case", () => {
         expect(allTools[0].tags).toBeTruthy();
         expect(allTools[0].link).toBeTruthy();
     });
+
+    it("Should be able to throws ConflictError, when user try save a existent title", async () => {
+        const toolRepository = new ToolRepositoryMemory();
+        const saveTool = new SaveTool(toolRepository)
+
+        const tool = await ToolFactory(toolRepository)
+
+        const newTool = saveTool.exec({
+            title: tool.title,
+            description: faker.random.words(5),
+            tags: [`${faker.word.noun()}`],
+            link: faker.internet.url(),
+        })
+
+        await expect(newTool).rejects.toThrow(ConflictError)
+    })
 });
